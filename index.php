e057f36fbba4d45324b38f3d21d83c24c81c9815
totoro-moon <flaviefrancois21@gmail.com>
2021-06-11 12:17:32 +0200

    Exercie_2_P1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 2 Partie 1</title>
+</head>
+<body>
+    <h1># exercice 2</h1>
+<p>Créez trois variables en php : nom, prénom, âge. Afficher ces variables dans une phrase (ex: Je m'appelle "nom prénom" et j'ai "age" ans.)
+</p>
+<?php
+$nom = 'MARTIN';
+$prenom = 'Camille';
+$age = 25;
+echo 'Bonjour ' .$prenom. ' ' .$nom. ', tu as ' .$age. ' ans.';
+?>
+</body>
+</html>
